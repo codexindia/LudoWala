80bd484b669cbdaf8291e6865d95d2ad7e4bdcf2
codexindia <sudiptobain8@gmail.com>
2024-08-24 01:57:15 +0530

    feat: Add API endpoint to get tournament list

--- app/Http/Controllers/Api/TournamentManager.php
+++ app/Http/Controllers/Api/TournamentManager.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Tournaments; // Add this line to import the Tournament class
 
 class TournamentManager extends Controller
 {
    public function getTournamentList(Request $request)
    {
-    
+    $tournament = Tournaments::where('status','live')->get();
+    return response()->json([
+        'status' => true,
+        'data' => $tournament,
+    ]);
    }
 }
--- database/settings/2024_08_23_101426_general_settings.php
+++ database/settings/2024_08_23_101426_general_settings.php
@@ -6,7 +6,7 @@ return new class extends SettingsMigration
 {
     public function up(): void
     {
-        $this->migrator->add('general.maintenance', 1);
+        $this->migrator->add('general.maintenance', 0);
         $this->migrator->add('general.forceUpdate', 1);
         $this->migrator->add('general.apkVersion', '1.0');
     }
