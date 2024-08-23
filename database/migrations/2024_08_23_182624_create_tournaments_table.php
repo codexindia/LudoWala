a64b3b9750a78d836dcd39482df0f46856fb238b
codexindia <sudiptobain8@gmail.com>
2024-08-24 01:39:39 +0530

    refactor: Update tournaments table schema
    - Rename 'name' column to 'title'
    - Update column names to follow camel case convention
    - Add new columns for round details, prizes, and bonuses
    - Remove 'is_active' column

--- database/migrations/2024_08_23_182624_create_tournaments_table.php
+++ database/migrations/2024_08_23_182624_create_tournaments_table.php
@@ -13,16 +13,33 @@ return new class extends Migration
     {
         Schema::create('tournaments', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->integer('first_prize');
-            $table->integer('assumed_winning_percent');
-            $table->integer('entry_fee');
-            $table->date('start_date');
-            $table->time('start_time');
-            
-            $table->integer('max_players');
-            $table->integer('entry_fee');
-            $table->boolean('is_active')->default(true);
+            $table->string('title');
+            $table->integer('entryFee');
+            $table->integer('maxPlayers');
+            $table->integer('totalRound');
+            $table->integer('roundInterval');
+            $table->integer('currentRound')->default(0);
+            $table->dateTime('registrationStartTime')->useCurrent();
+            $table->dateTime('registrationEndTime');
+            $table->dateTime('startTime');
+            $table->dateTime('nextRoundTime');
+            $table->enum('status',['live','upcoming','complete'])->default('live');
+            $table->string('1stPrize');
+            $table->string('2ndPrize');
+            $table->string('3rdPrize');
+            $table->string('4thPrize');
+            //$table->boolean('is_active')->default(true);
+            $table->integer('1stRoundBonus')->default(0);
+            $table->integer('2ndRoundBonus')->default(0);
+            $table->integer('3rdRoundBonus')->default(0);
+            $table->integer('4thRoundBonus')->default(0);
+            $table->integer('5thRoundBonus')->default(0);
+            $table->integer('6thRoundBonus')->default(0);
+            $table->integer('7thRoundBonus')->default(0);
+            $table->integer('8thRoundBonus')->default(0);
+            $table->integer('9thRoundBonus')->default(0);
+
+          
             $table->timestamps();
         });
     }
