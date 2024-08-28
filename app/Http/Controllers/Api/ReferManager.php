019e10e804a89fcf956d7bf6fd6f95277dfc5382
codexindia <sudiptobain8@gmail.com>
2024-08-29 01:09:31 +0530

    refactor: Update profile picture URLs and myReferrals API endpoint in ReferManager.php

--- app/Http/Controllers/Api/ReferManager.php
+++ app/Http/Controllers/Api/ReferManager.php
@@ -31,7 +31,7 @@ class ReferManager extends Controller
         $leaderboard->transform(function ($item) {
            
             $item->total_deposit ??= 0;
-            $item->profilePic = "https://api.dicebear.com/9.x/micah/png?backgroundColor=#b6e3f4,#c0aede,#d1d4&seed=".$item->fname.'+'.$item->lname;
+            $item->profilePic = "https://api.dicebear.com/9.x/initials/png?seed=".$item->fname.'+'.$item->lname;
             return $item;
         });
 
@@ -66,7 +66,7 @@ class ReferManager extends Controller
             // ->orderBy('total_winning', 'DESC')
             // ->paginate(10);
         $referrals->transform(function ($item) {
-            $item->profilePic = "https://api.dicebear.com/9.x/micah/png?backgroundColor=#b6e3f4,#c0aede,#d1d4&seed=".$item->fname.'+'.$item->lname;
+            $item->profilePic = "https://api.dicebear.com/9.x/initials/png?seed=".$item->fname.'+'.$item->lname;
           //  $item->rank = $item->getRank();
             return $item;
         });
