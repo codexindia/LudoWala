55b56e57d4e2780673c8723ade7e14053f085ec9
codexindia <sudiptobain8@gmail.com>
2024-08-28 12:57:45 +0530

    feat: Return credit balance after saving new user in AuthManager

--- app/Http/Controllers/Api/AuthManager.php
+++ app/Http/Controllers/Api/AuthManager.php
@@ -114,7 +114,7 @@ class AuthManager extends Controller
                 $newuser->mobileNumber = $request->mobileNumber;
                // $newuser->referCode = $referCode;
                 $newuser->save();
-                creditBal($newuser->id, 100, 0, "bonus_wallet", "Welcome Bonus Credited");
+              return  creditBal($newuser->id, 100, 0, "bonus_wallet", "Welcome Bonus Credited");
           
                 $token = $newuser->createToken('auth_token')->plainTextToken;
                 return response()->json([
