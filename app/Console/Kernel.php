d65633030a90e6b5d6f259080837d1c224afd5ad
codexindia <sudiptobain8@gmail.com>
2024-09-21 14:38:14 +0530

    Refactor command description to "Change TimeOut By 20 sec"

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->command(changeTurn::class)->everyThirtySeconds();
+         $schedule->command(changeTurn::class)->everyTwentySeconds();
     }
 
     /**
