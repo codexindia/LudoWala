42497c1c0469d2cdb267987db96507f0b7083df2
codexindia <sudiptobain8@gmail.com>
2024-09-21 14:50:17 +0530

    Refactor command description to update turn change frequency

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->command(changeTurn::class)->everyFiveSeconds();
+         $schedule->command(changeTurn::class)->everySecond();
     }
 
     /**
